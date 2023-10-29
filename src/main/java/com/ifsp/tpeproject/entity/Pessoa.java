577d6671dee9f178dab8760aaccb52f45c7f6fe7
Lucas Briz <lucasbriz@outlook.com>
2023-10-29 18:30:33 -0300

    create pessoa class

---
+++ src/main/java/com/ifsp/tpeproject/entity/Pessoa.java
@@ -0,0 +1,60 @@
+package com.ifsp.tpeproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+import lombok.Data;
+
+@Entity
+public class Pessoa {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private int age;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(final Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(final int age) {
+    this.age = age;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Pessoa pessoa = (Pessoa) o;
+    return age == pessoa.age && Objects.equals(id, pessoa.id) && Objects.equals(
+        name, pessoa.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, age);
+  }
+}
