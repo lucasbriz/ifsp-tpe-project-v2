89f37709dbf7e64aad4105bfb46df07050dbaf4f
Lucas Briz <lucasbriz@outlook.com>
2023-10-29 18:13:56 -0300

    initial configuration

---
+++ src/main/java/com/ifsp/tpeproject/TpeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.tpeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsp/tpeproject/TpeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.tpeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
